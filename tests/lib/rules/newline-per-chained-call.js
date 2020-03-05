33b48dd8ed3f7746e88bde6b7bfd5acdef595470
Kevin Ghadyani <Saturn2888@gmail.com>
2020-03-05 03:29:24 -0600

    Update: Re-added regressed behavior for enforcing a max total chained depth

--- tests/lib/rules/newline-per-chained-call.js
+++ tests/lib/rules/newline-per-chained-call.js
@@ -26,51 +26,55 @@ ruleTester.run("newline-per-chained-call", rule, {
             ignoreChainWithDepth: 8
         }]
     }],
+
+    /*
+     * ignoreChainWithDepth: 2 (default)
+     */
     invalid: [{
         code: "_\n.chain({}).map(foo).filter(bar).value();",
         output: "_\n.chain({}).map(foo)\n.filter(bar)\n.value();",
         errors: [{
-            messageId: "expected", data: { callee: ".filter" }
+            messageId: "expectedLineBreak", data: { propertyName: ".filter" }
         }, {
-            messageId: "expected", data: { callee: ".value" }
+            messageId: "expectedLineBreak", data: { propertyName: ".value" }
         }]
     }, {
         code: "_\n.chain({})\n.map(foo)\n.filter(bar).value();",
         output: "_\n.chain({})\n.map(foo)\n.filter(bar)\n.value();",
         errors: [{
-            messageId: "expected", data: { callee: ".value" }
+            messageId: "expectedLineBreak", data: { propertyName: ".value" }
         }]
     }, {
         code: "a().b().c().e.d()",
         output: "a().b()\n.c().e.d()",
         errors: [{
-            messageId: "expected", data: { callee: ".c" }
+            messageId: "expectedLineBreak", data: { propertyName: ".c" }
         }]
     }, {
         code: "a.b.c().e().d()",
         output: "a.b.c().e()\n.d()",
         errors: [{
-            messageId: "expected", data: { callee: ".d" }
+            messageId: "expectedLineBreak", data: { propertyName: ".d" }
         }]
     }, {
         code: "_.chain({}).map(a).value(); ",
         output: "_.chain({}).map(a)\n.value(); ",
         errors: [{
-            messageId: "expected", data: { callee: ".value" }
+            messageId: "expectedLineBreak", data: { propertyName: ".value" }
         }]
     }, {
-        code: "var a = m1.m2();\n var b = m1.m2().m3().m4().m5();",
-        output: "var a = m1.m2();\n var b = m1.m2().m3()\n.m4()\n.m5();",
+        code: "var a = m1.m2();\nvar b = m1.m2().m3().m4().m5();",
+        output: "var a = m1.m2();\nvar b = m1.m2().m3()\n.m4()\n.m5();",
         errors: [{
-            messageId: "expected", data: { callee: ".m4" }
+            messageId: "expectedLineBreak", data: { propertyName: ".m4" }
         }, {
-            messageId: "expected", data: { callee: ".m5" }
+            messageId: "expectedLineBreak", data: { propertyName: ".m5" }
         }]
     }, {
-        code: "var a = m1.m2();\n var b = m1.m2().m3()\n.m4().m5();",
-        output: "var a = m1.m2();\n var b = m1.m2().m3()\n.m4()\n.m5();",
+        code: "var a = m1.m2();\nvar b = m1.m2().m3()\n.m4().m5();",
+        output: "var a = m1.m2();\nvar b = m1.m2().m3()\n.m4()\n.m5();",
         errors: [{
-            messageId: "expected", data: { callee: ".m5" }
+            messageId: "expectedLineBreak", data: { propertyName: ".m5" }
         }]
     }, {
         code: "var a = m1().m2\n.m3().m4().m5().m6().m7();",
@@ -79,9 +83,9 @@ ruleTester.run("newline-per-chained-call", rule, {
             ignoreChainWithDepth: 3
         }],
         errors: [{
-            messageId: "expected", data: { callee: ".m6" }
+            messageId: "expectedLineBreak", data: { propertyName: ".m6" }
         }, {
-            messageId: "expected", data: { callee: ".m7" }
+            messageId: "expectedLineBreak", data: { propertyName: ".m7" }
         }]
     }, {
         code: [
@@ -145,9 +149,9 @@ ruleTester.run("newline-per-chained-call", rule, {
             ".end();"
         ].join("\n"),
         errors: [{
-            messageId: "expected", data: { callee: ".on" }
+            messageId: "expectedLineBreak", data: { propertyName: ".on" }
         }, {
-            messageId: "expected", data: { callee: ".end" }
+            messageId: "expectedLineBreak", data: { propertyName: ".end" }
         }]
     }, {
         code: [
@@ -163,34 +167,316 @@ ruleTester.run("newline-per-chained-call", rule, {
             "    'method4']()"
         ].join("\n"),
         errors: [{
-            messageId: "expected", data: { callee: "['method' + n]" }
+            messageId: "expectedLineBreak", data: { propertyName: "['method' + n]" }
         }, {
-            messageId: "expected", data: { callee: "[aCondition ?" }
+            messageId: "expectedLineBreak", data: { propertyName: "[aCondition ?" }
         }]
-    }, {
+    },
+
+    /*
+     * ignoreChainWithDepth: 1
+     */
+    {
         code: "foo.bar()['foo' + \u2029 + 'bar']()",
         output: "foo.bar()\n['foo' + \u2029 + 'bar']()",
         options: [{ ignoreChainWithDepth: 1 }],
-        errors: [{ messageId: "expected", data: { callee: "['foo' + " } }]
+        errors: [{ messageId: "expectedLineBreak", data: { propertyName: "['foo' + " } }]
     }, {
         code: "foo.bar()[(biz)]()",
         output: "foo.bar()\n[(biz)]()",
         options: [{ ignoreChainWithDepth: 1 }],
-        errors: [{ messageId: "expected", data: { callee: "[biz]" } }]
+        errors: [{ messageId: "expectedLineBreak", data: { propertyName: "[biz]" } }]
     }, {
         code: "(foo).bar().biz()",
         output: "(foo).bar()\n.biz()",
         options: [{ ignoreChainWithDepth: 1 }],
-        errors: [{ messageId: "expected", data: { callee: ".biz" } }]
+        errors: [{ messageId: "expectedLineBreak", data: { propertyName: ".biz" } }]
     }, {
         code: "foo.bar(). /* comment */ biz()",
         output: "foo.bar()\n. /* comment */ biz()",
         options: [{ ignoreChainWithDepth: 1 }],
-        errors: [{ messageId: "expected", data: { callee: ".biz" } }]
+        errors: [{ messageId: "expectedLineBreak", data: { propertyName: ".biz" } }]
     }, {
         code: "foo.bar() /* comment */ .biz()",
         output: "foo.bar() /* comment */ \n.biz()",
         options: [{ ignoreChainWithDepth: 1 }],
-        errors: [{ messageId: "expected", data: { callee: ".biz" } }]
+        errors: [{ messageId: "expectedLineBreak", data: { propertyName: ".biz" } }]
+    },
+
+    /*
+     * maxTotalChainDepth: 1
+     */
+    {
+        code: "_\n.chain({}).map(foo).filter(bar).value();",
+        output: "_\n.chain({})\n.map(foo)\n.filter(bar)\n.value();",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".map" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".filter" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".value" }
+        }]
+    }, {
+        code: "_\n.chain({})\n.map(foo)\n.filter(bar).value();",
+        output: "_\n.chain({})\n.map(foo)\n.filter(bar)\n.value();",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".value" }
+        }]
+    }, {
+        code: "a().b().c().e.d()",
+        output: "a()\n.b()\n.c()\n.e\n.d()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".b" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".c" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".e" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".d" }
+        }]
+    }, {
+        code: "a.b.c().e().d()",
+        output: "a\n.b\n.c()\n.e()\n.d()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".b" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".c" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".e" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".d" }
+        }]
+    }, {
+        code: "_.chain({}).map(a).value(); ",
+        output: "_\n.chain({})\n.map(a)\n.value(); ",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".chain" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".map" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".value" }
+        }]
+    }, {
+        code: "var a = m1.m2();\nvar b = m1.m2().m3().m4().m5();",
+        output: "var a = m1.m2();\nvar b = m1\n.m2()\n.m3()\n.m4()\n.m5();",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".m2" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m3" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m4" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m5" }
+        }]
+    }, {
+        code: "var a = m1.m2();\nvar b = m1.m2().m3()\n.m4().m5();",
+        output: "var a = m1.m2();\nvar b = m1\n.m2()\n.m3()\n.m4()\n.m5();",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".m2" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m3" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m5" }
+        }]
+    }, {
+        code: "var a = m1().m2\n.m3().m4().m5().m6().m7();",
+        output: "var a = m1()\n.m2\n.m3()\n.m4()\n.m5()\n.m6()\n.m7();",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".m2" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m4" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m5" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m6" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".m7" }
+        }]
+    }, {
+        code: [
+            "http.request({",
+            "    // Param",
+            "    // Param",
+            "    // Param",
+            "}).on('response', function(response) {",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "}).on('error', function(error) {",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "}).end();"
+        ].join("\n"),
+        output: [
+            "http",
+            ".request({",
+            "    // Param",
+            "    // Param",
+            "    // Param",
+            "})",
+            ".on('response', function(response) {",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "    // Do something with response.",
+            "})",
+            ".on('error', function(error) {",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "    // Do something with error.",
+            "})",
+            ".end();"
+        ].join("\n"),
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".request" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".on" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".on" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".end" }
+        }]
+    }, {
+        code: [
+            "anObject.method1().method2()['method' + n]()[aCondition ?",
+            "    'method3' :",
+            "    'method4']()"
+        ].join("\n"),
+        output: [
+            "anObject",
+            ".method1()",
+            ".method2()",
+            "['method' + n]()",
+            "[aCondition ?",
+            "    'method3' :",
+            "    'method4']()"
+        ].join("\n"),
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{
+            messageId: "expectedLineBreak", data: { propertyName: ".method1" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: ".method2" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: "['method' + n]" }
+        }, {
+            messageId: "expectedLineBreak", data: { propertyName: "[aCondition ?" }
+        }]
+    }, {
+        code: "foo.bar()['foo' + \u2029 + 'bar']()",
+        output: "foo\n.bar()\n['foo' + \u2029 + 'bar']()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [
+            { messageId: "expectedLineBreak", data: { propertyName: ".bar" } },
+            { messageId: "expectedLineBreak", data: { propertyName: "['foo' + " } }
+        ]
+    }, {
+        code: "foo.bar()[(biz)]()",
+        output: "foo\n.bar()\n[(biz)]()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [
+            { messageId: "expectedLineBreak", data: { propertyName: ".bar" } },
+            { messageId: "expectedLineBreak", data: { propertyName: "[biz]" } }
+        ]
+    }, {
+        code: "(foo).bar().biz()",
+        output: "(foo)\n.bar()\n.biz()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [
+            { messageId: "expectedLineBreak", data: { propertyName: ".bar" } },
+            { messageId: "expectedLineBreak", data: { propertyName: ".biz" } }
+        ]
+    }, {
+        code: "foo.bar(). /* comment */ biz()",
+        output: "foo\n.bar()\n. /* comment */ biz()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [
+            { messageId: "expectedLineBreak", data: { propertyName: ".bar" } },
+            { messageId: "expectedLineBreak", data: { propertyName: ".biz" } }
+        ]
+    }, {
+        code: "foo.bar() /* comment */ .biz()",
+        output: "foo\n.bar() /* comment */ \n.biz()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [
+            { messageId: "expectedLineBreak", data: { propertyName: ".bar" } },
+            { messageId: "expectedLineBreak", data: { propertyName: ".biz" } }
+        ]
+    }, {
+        code: "foo\n.bar() /* comment */ .biz()",
+        output: "foo\n.bar() /* comment */ \n.biz()",
+        options: [{ maxTotalChainDepth: 1 }],
+        errors: [{ messageId: "expectedLineBreak", data: { propertyName: ".biz" } }]
+    },
+
+    /*
+     * maxTotalChainDepth: 2
+     */
+    {
+        code: "foo.bar()['foo' + \u2029 + 'bar']()",
+        output: null,
+        options: [{ maxTotalChainDepth: 2 }],
+        errors: []
+    }, {
+        code: "foo.bar()[(biz)]()",
+        output: null,
+        options: [{ maxTotalChainDepth: 2 }],
+        errors: []
+    }, {
+        code: "(foo).bar().biz()",
+        output: null,
+        options: [{ maxTotalChainDepth: 2 }],
+        errors: []
+    }, {
+        code: "foo.bar(). /* comment */ biz()",
+        output: null,
+        options: [{ maxTotalChainDepth: 2 }],
+        errors: []
+    }, {
+        code: "foo.bar() /* comment */ .biz()",
+        output: null,
+        options: [{ maxTotalChainDepth: 2 }],
+        errors: []
+    }, {
+        code: "foo\n.bar() /* comment */ .biz()",
+        output: null,
+        options: [{ maxTotalChainDepth: 2 }],
+        errors: []
     }]
 });
